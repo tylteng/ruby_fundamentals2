eca6c686fbda53992f5de66c9c7e680c6c0c2b0d
tylteng <teng.ty4@gmail.com>
2017-10-31 16:54:41 -0400

    completed up to exercise 5

---
+++ index.rb
@@ -0,0 +1,60 @@
+# Exercise 1
+def double(number)
+  return 2 * number
+end
+
+new_number = double(3)
+puts new_number
+
+new_number = double(6)
+puts new_number
+
+new_number = double(100)
+puts new_number
+
+puts "-" * 30
+
+# Exercise 2
+def negative?(number)
+  if number > 0
+    return true
+  else
+    return false
+  end
+end
+
+puts negative?(-100)
+puts negative?(900)
+puts negative?(-1)
+
+puts "-" * 30
+
+# Exercise 3
+puts "Plug in a number."
+number_yours = gets.chomp.to_i
+
+def is_even?(number)
+  if number % 2 == 0
+    puts "Your number is even."
+  else
+    puts "Your number is odd."
+  end
+end
+
+puts is_even?(number_yours)
+
+puts "-" * 30
+
+# Exercise 4
+puts "What is your name?"
+name = gets.chomp
+
+def length(name)
+  if name.length < 8
+    return false
+  else
+    return true
+  end
+end
+
+puts length('name')
